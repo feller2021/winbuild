9a211cd3940fe0b3a76123943fe19e6b1d33bcaa
Feller <iamliurenjie@gmail.com>
2023-03-07 21:01:34 +0800

    Update ty.py

--- ty.py
+++ ty.py
@@ -31,13 +31,15 @@ def bulid():
                 fields={'name': 'ISO.ISO', 'puid': SCKEY,'_token': SCKEY2,
                         'file': (dataname, open(dizhi, 'rb'))}
             )
+            time.sleep(6)  
             headers = {
                 "Content-Type": m.content_type,
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.63",
                 "Connection": 'close'
             }
             r = requests.post(url, data=m,
-                              headers=headers, timeout=120)
+                              headers=headers, timeout=(7,12))
+           time.sleep(6) 
             res=r.text
             # print(res)
             jsonobj = json.loads(res)
