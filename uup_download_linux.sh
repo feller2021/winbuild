70244a477a9eb09de8065134b6589b02c5cfbee7
feller2021 <100558543+feller2021@users.noreply.github.com>
2023-04-06 13:44:04 +0800

    Add files via upload

--- uup_download_linux.sh
+++ uup_download_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Generated on 2023-03-22 03:06:08 GMT
- 
+#Generated on 2023-04-06 05:41:20 GMT
+
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
 # then you can do this by configuring the all_proxy environment variable.
@@ -12,54 +12,54 @@
 # More information how to use this can be found at:
 # https://aria2.github.io/manual/en/html/aria2c.html#cmdoption-all-proxy
 # https://aria2.github.io/manual/en/html/aria2c.html#environment
- 
+
 export all_proxy=""
- 
+
 # End of proxy configuration
- 
+
 for prog in aria2c cabextract wimlib-imagex chntpw; do
   which $prog &>/dev/null 2>&1 && continue;
- 
+
   echo "$prog does not seem to be installed"
   echo "Check the readme.unix.md for details"
   exit 1
 done
- 
+
 mkiso_present=0
 which genisoimage &>/dev/null && mkiso_present=1
 which mkisofs &>/dev/null && mkiso_present=1
- 
+
 if [ $mkiso_present -eq 0 ]; then
   echo "genisoimage nor mkisofs does seem to be installed"
   echo "Check the readme.unix.md for details"
   exit 1
 fi
- 
+
 destDir="UUPs"
 tempScript="aria2_script.$RANDOM.txt"
- 
-echo "Downloading converters..."
 
+echo "Downloading converters..."
+aria2c --no-conf --log-level=info --log="aria2_download.log" -x16 -s16 -j5 --allow-overwrite=true --auto-file-renaming=false -d"files" -i"files/converter_multi"
 if [ $? != 0 ]; then
   echo "We have encountered an error while downloading files."
   exit 1
 fi
- 
+
 echo ""
 echo "Retrieving aria2 script..."
-aria2c --no-conf --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=2326143b-ab3d-484a-90aa-3bc64e1a7c0c&pack=zh-cn&edition=corecountryspecific;professional&aria2=2"
+aria2c --no-conf --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "http://uupdump.net/get.php?id=053b2564-d160-40ee-998f-87ce912fa87b&pack=zh-cn&edition=professional&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
 fi
- 
+
 detectedError=`grep '#UUPDUMP_ERROR:' "$tempScript" | sed 's/#UUPDUMP_ERROR://g'`
 if [ ! -z $detectedError ]; then
     echo "Unable to retrieve data from Windows Update servers. Reason: $detectedError"
     echo "If this problem persists, most likely the set you are attempting to download was removed from Windows Update servers."
     exit 1
 fi
- 
+
 echo ""
 echo "Attempting to download files..."
 aria2c --no-conf --log-level=info --log="aria2_download.log" -x16 -s16 -j5 -c -R -d"$destDir" -i"$tempScript"
@@ -67,7 +67,7 @@ if [ $? != 0 ]; then
   echo "We have encountered an error while downloading files."
   exit 1
 fi
- 
+
 echo ""
 if [ -e ./files/convert.sh ]; then
   chmod +x ./files/convert.sh
